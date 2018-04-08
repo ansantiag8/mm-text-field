a5c8120d34f73c0dc5ff60f4e7bdca084f03310c
Santiago Rodriguez <an.santiago@tutanota.com>
2018-04-08 18:27:48 -0500

    se realizan correcciones y respectivas integraciones con animation y layout

--- src/text-field.js
+++ src/text-field.js
@@ -1,3 +1,32 @@
-// import '../node_modules/mm-animations/src/animations';
 import 'mm-animations';
 import './style/text-field.css';
+
+// TODO: pendiente al cargar el componente, validar que este diligenciada informacion
+
+function textField() {
+  const eventChange = (nodeHtml) => {
+    const input = nodeHtml.children[0];
+
+    input.addEventListener('change', (event) => {
+      const { target: { value: valueComp } } = event;
+
+      if (valueComp && valueComp !== '') {
+        input.classList.add('mm-input-not-empty');
+      }
+    });
+  };
+
+  const constructor = () => {
+    const components = document.querySelectorAll('.mm-text-field');
+
+    components.forEach((comp) => {
+      eventChange(comp);
+    });
+  };
+
+  constructor();
+}
+
+textField();
+
+module.export = { textField };
